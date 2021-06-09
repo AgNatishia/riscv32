0f86888dbe426dbe03cd18c1784b686b652cf505
Stephen Clarke <scl@xilinx.com>
2021-06-09 15:39:56 +0100

    Added new test programs for testing widgets

---
+++ test_programs/blink_upper_2_LEDs/blink_upper_2_LEDs.c
@@ -0,0 +1,22 @@
+#include <stdint.h>
+
+const int value = 0;
+const int tristate = 1;
+int32_t volatile * const buttons = (int32_t*) 0x40010000;
+int32_t volatile * const LEDs    = (int32_t*) 0x40020000;
+
+int main()
+{
+  // Turn off all LEDs
+  LEDs[tristate] = 0;
+
+  while(1)
+  {
+    for(int i = 0; i < 4; i++)
+    {
+      LEDs[value] = 4*i;
+    }
+  }
+
+  return 0;
+}
